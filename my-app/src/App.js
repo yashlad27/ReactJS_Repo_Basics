31e5e3928d99e45a29f5d8737ae124e2ede3dfd4
Yash Lad <yashlad727@gmail.com>
2022-11-19 09:37:40 +0530

    class V/s Functional component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learning React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +42,8 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
